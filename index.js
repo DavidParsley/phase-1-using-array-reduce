146724a571b714c57541df801f6714f3f13bb066
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-10-02 16:58:40 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((number, batch) => {
+    return number + batch;
+  }, 0);
+  console.log(totalBatteries)
